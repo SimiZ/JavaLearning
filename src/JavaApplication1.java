c3151cbf2d566666a69136685a7c49cb94b90aca
SimiZ <Simas@158.129.20.77>
2014-02-13 15:30:29 +0200

    Visual changes

--- src/JavaApplication1.java
+++ src/JavaApplication1.java
@@ -1,23 +1,22 @@
 public class JavaApplication1 {
-	public static void main(String[] args) {
-	
-		int age = 1;
-                int grown = 15;
-
-		switch(age) {
-			case 1:
-				System.out.println("One");
-				break;
-			case 2:
-				System.out.println("Two");
-				break;
-			case 3:
-				System.out.println("Three");
-				break;
-			default:
-				System.out.println("Other");
-				break;
-		}
-
-	}
+    public static void main(String[] args) {
+        
+        int age = 1;
+        int grown = 15;
+        
+        switch(age) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Other");
+                break;
+        }
+    }
 }
