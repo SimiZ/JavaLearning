5073238fc0681d4bcfb37b5af2392113332a3504
SimiZ <Simas@158.129.20.77>
2014-02-13 14:58:14 +0200

    new commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class JavaApplication1 {
+	public static void main(String[] args) {
+	
+		int age = 4;
+
+		switch(age) {
+			case 1:
+				System.out.println("One");
+				break;
+			case 2:
+				System.out.println("Two");
+				break;
+			case 3:
+				System.out.println("Three");
+				break;
+			default:
+				System.out.println("Other");
+				break;
+		}
+
+	}
+}
